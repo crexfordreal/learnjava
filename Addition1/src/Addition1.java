9e40a2fe227dd2368984693a2725a5c59f1b9ba8
crexfordreal <crexford@realnetworks.com>
2012-09-16 10:12:20 -0700

    Adding Addition1 Project

---
+++ Addition1/src/Addition1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Addition1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner( System.in );
+		
+		int number1 = 0;
+		int number2 = 0;
+		int sum = 0;
+		
+		System.out.print( "Please enter the first number: ");
+		number1 = input.nextInt();
+		
+		System.out.print( "Please enter the second number: ");
+		number2 = input.nextInt();
+		
+		sum = number1 + number2;
+		System.out.printf(format, args)
+		
+
+	}
+
+}
