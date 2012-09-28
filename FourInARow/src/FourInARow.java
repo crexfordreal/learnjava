b53c61215057dcf01468c5155356e14d6e3d260d
crexfordreal <crexford@realnetworks.com>
2012-09-27 17:15:32 -0700

    Adding new FourInARow Project

---
+++ FourInARow/src/FourInARow.java
@@ -0,0 +1,14 @@
+
+public class FourInARow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//System.out.println("1 2 3 4");
+		//System.out.print("1 2 3 4");
+		System.out.printf("%d %d %d %d", 1, 2, 3, 4);
+
+	}
+
+}
