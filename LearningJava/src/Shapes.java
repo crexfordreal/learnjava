6d9acc95f78652f4ba616d0a9999db58101be98d
crexfordreal <crexford@realnetworks.com>
2012-10-08 16:49:39 -0700

    Shapes added

---
+++ LearningJava/src/Shapes.java
@@ -0,0 +1,19 @@
+// Exercise 2.18
+public class Shapes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.printf( "%10s%10s%10s%10s\n", "*********", "***", "*", "*" );
+		System.out.println( " *       *     *     *      ***       * *");
+		System.out.println( " *       *    *       *    *****     *   *");
+		System.out.println( " *       *    *       *      *      *     *");
+		System.out.println( " *       *    *       *      *     *       *");
+		System.out.println( " *       *    *       *      *      *     *");
+		System.out.println( " *       *    *       *      *       *   *");
+		System.out.println( " *       *     *     *       *        * *");
+		System.out.printf( "%10s%10s%10s%10s\n", "*********", "***", "*", "*" );
+	}
+
+}
